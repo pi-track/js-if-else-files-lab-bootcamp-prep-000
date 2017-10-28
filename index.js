f0252722f40329692374051b03e3b3b07886cc2e
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-10-28 22:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 }
+else {
+  console.log("expected name to be susan")
+}
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
